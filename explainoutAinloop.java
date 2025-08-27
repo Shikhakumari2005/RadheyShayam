47848510e2289866f391708df37a0980bdff564c
unknown <siragenius1@gmail.com>
2025-08-27 14:09:39 +0530

    sum function

--- explainoutAinloop.java
+++ explainoutAinloop.java
@@ -6,13 +6,7 @@ public class explainoutAinloop {
         for (int i = 0; i < arr.length; i++) {
            sum += arr[i];
            System.out.println("After adding " + arr[i] + ", sum is: " + sum);
-        }
 
-        int[] arr1 = {1, 2, 3, 4};
-        int sum1 = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-        sum1 += arr[i];
         }
         System.out.println("Final sum is: " + sum);
 
