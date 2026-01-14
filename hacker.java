fd05bc8de7ef34ab139ed94985de1673b4bca7ae
unknown <siragenius1@gmail.com>
2026-01-14 22:30:02 +0530

    fibbanoic

---
+++ hacker.java
@@ -0,0 +1,11 @@
+import java.util;
+public class hacker{
+    public static void fibbaconic(int a ,int b){
+        int c=a+b;
+        fibbacoinc()
+
+    }
+    public static void main(String[] args){
+
+    }
+}
