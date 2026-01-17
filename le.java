8d1b112264de2651a12bbbf91b33d43456a2b998
unknown <siragenius1@gmail.com>
2026-01-17 23:26:46 +0530

    levelorder

---
+++ le.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class le {
+
+    // Level Order Traversal
+    static void levelOrder(Node root) {
+        if (root == null) return;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            Node curr = q.poll();
+            System.out.print(curr.data + " ");
+
+            if (curr.left != null)
+                q.add(curr.left);
+
+            if (curr.right != null)
+                q.add(curr.right);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        /*
+                 1
+               /   \
+              2     3
+             / \   / \
+            4   5 6   7
+        */
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println("Level Order Traversal:");
+        levelOrder(root);
+    }
+}
